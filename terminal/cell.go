8d2a4f8f38af318c16ac45ddf8a2fea7ee904566
Wang Qi <ericwq057@qq.com>
2022-04-13 08:16:58 +0800

    change color type from uint to uint32. add SGR() method.

--- terminal/cell.go
+++ terminal/cell.go
@@ -1,25 +1,31 @@
 package terminal
 
+import (
+	"fmt"
+	"strings"
+)
+
 const (
-	Bold uint = iota
+	Bold uint32 = iota
 	Faint
 	Italic
 	Underlined
 	Blink
+	RapidBlink // this one is added by SGR
 	Inverse
 	Invisible
 	SIZE
 )
 
-const TrueColorMask uint = 0x80000000
+const TrueColorMask uint32 = 0x1000000
 
 type Renditions struct {
-	foregroundColor uint
-	backgroundColor uint
-	attributes      uint
+	foregroundColor uint32
+	backgroundColor uint32
+	attributes      uint32
 }
 
-func (r *Renditions) SetForegroundColor(color uint) {
+func (r *Renditions) SetForegroundColor(color uint32) {
 	if color <= 255 {
 		r.foregroundColor = 30 + color
 	} else if isTrueColor(color) {
@@ -27,7 +33,7 @@ func (r *Renditions) SetForegroundColor(color uint) {
 	}
 }
 
-func (r *Renditions) SetBackgroundColor(color uint) {
+func (r *Renditions) SetBackgroundColor(color uint32) {
 	if color <= 255 {
 		r.backgroundColor = 40 + color
 	} else if isTrueColor(color) {
@@ -35,7 +41,7 @@ func (r *Renditions) SetBackgroundColor(color uint) {
 	}
 }
 
-func (r *Renditions) SetRendition(color uint) {
+func (r *Renditions) SetRendition(color uint32) {
 	if color == 0 {
 		r.ClearAttributes()
 		r.foregroundColor = 0
@@ -43,6 +49,8 @@ func (r *Renditions) SetRendition(color uint) {
 		return
 	}
 
+	// SGR (Select Graphic Rendition) parameters in
+	// https://en.wikipedia.org/wiki/ANSI_escape_code
 	switch color {
 	case 39: // Sets the foreground color to the user's configured default text color
 		r.foregroundColor = 0
@@ -83,18 +91,60 @@ func (r *Renditions) SetRendition(color uint) {
 }
 
 func (r *Renditions) SGR() string {
-	return ""
+	var ret strings.Builder
+
+	ret.WriteString("\033[0") // starrts with reset rendition
+
+	if r.GetAttributes(Bold) {
+		ret.WriteString(";1")
+	}
+	if r.GetAttributes(Italic) {
+		ret.WriteString(";3")
+	}
+	if r.GetAttributes(Underlined) {
+		ret.WriteString(";4")
+	}
+	if r.GetAttributes(Blink) {
+		ret.WriteString(";5")
+	}
+	if r.GetAttributes(Inverse) {
+		ret.WriteString(";7")
+	}
+	if r.GetAttributes(Invisible) {
+		ret.WriteString(";8")
+	}
+
+	if r.foregroundColor > 0 {
+		if isTrueColor(r.foregroundColor) { // 24 bit color
+			fmt.Fprintf(&ret, ";38:2;%d;%d;%d", (r.foregroundColor>>16)&0xff, (r.foregroundColor>>8)&0xff, r.foregroundColor&0xff)
+		} else if r.foregroundColor > 37 { // use 256-color set
+			fmt.Fprintf(&ret, ";38;5;%d", r.foregroundColor-30)
+		} else { // ANSI foreground color
+			fmt.Fprintf(&ret, ";%d", r.foregroundColor)
+		}
+	}
+	if r.backgroundColor > 0 {
+		if isTrueColor(r.backgroundColor) {
+			fmt.Fprintf(&ret, ";48;2;%d;%d;%d", (r.backgroundColor>>16)&0xff, (r.backgroundColor>>8)&0xff, r.backgroundColor&0xff)
+		} else if r.backgroundColor > 47 {
+			fmt.Fprintf(&ret, ";48;5;%d", r.backgroundColor-40)
+		} else {
+			fmt.Fprintf(&ret, ";%d", r.backgroundColor)
+		}
+	}
+	ret.WriteString("m")
+	return ret.String()
 }
 
-func (r *Renditions) SetAttributes(attr uint, on bool) {
-	if on {
+func (r *Renditions) SetAttributes(attr uint32, turnOn bool) {
+	if turnOn {
 		r.attributes |= (1 << attr)
 	} else {
 		r.attributes &= ^(1 << attr)
 	}
 }
 
-func (r *Renditions) GetAttributes(attr uint) bool {
+func (r *Renditions) GetAttributes(attr uint32) bool {
 	return (r.attributes & (1 << attr)) > 0
 }
 
@@ -102,10 +152,10 @@ func (r *Renditions) ClearAttributes() {
 	r.attributes = 0
 }
 
-func makeTrueColor(r, g, b uint) uint {
+func makeTrueColor(r, g, b uint32) uint32 {
 	return TrueColorMask | (r << 16) | (g << 8) | b
 }
 
-func isTrueColor(color uint) bool {
-	return TrueColorMask&color != 0
+func isTrueColor(color uint32) bool {
+	return color&TrueColorMask != 0
 }
