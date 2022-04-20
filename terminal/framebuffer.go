a5dad8bc7c97395e02a63575017500704471b942
ericwq <ericwq057@qq.com>
2022-04-20 19:57:20 +0800

    add GetRows(); GetCell(); GetRow(); add InsertLine(); DeleteLine(); erase(); insert().

--- terminal/framebuffer.go
+++ terminal/framebuffer.go
@@ -24,8 +24,96 @@ func NewFramebuffer(width, height int) *Framebuffer {
 	return &fb
 }
 
-func (fb *Framebuffer) newRow(width, height int) *Row {
+func (fb *Framebuffer) newRow() *Row {
 	w := fb.DS.GetWidth()
 	bgColor := fb.DS.GetBackgroundRendition()
 	return NewRow(w, uint32(bgColor))
+}
+
+func (fb *Framebuffer) GetRows() []Row { return fb.rows }
+
+// there is no get_mutable_row() counterpart in go
+func (fb *Framebuffer) GetRow(row int) *Row {
+	if row == -1 {
+		row = fb.DS.GetCursorRow()
+	}
+	return &(fb.rows[row])
+}
+
+// there is no get_mutable_cell counterpart in go
+func (fb *Framebuffer) GetCell(row, col int) *Cell {
+	if row == -1 {
+		row = fb.DS.GetCursorRow()
+	}
+	if col == -1 {
+		col = fb.DS.GetCursorCol()
+	}
+	return fb.rows[row].At(col)
+}
+
+func (fb *Framebuffer) InsertLine(beforeRow int, count int) {
+	// invalide beforeRow
+	if beforeRow < fb.DS.GetScrollingRegionTopRow() || beforeRow > fb.DS.GetScrollingRegionBottomRow()+1 {
+		return
+	}
+
+	maxRoll := fb.DS.GetScrollingRegionBottomRow() + 1 - beforeRow
+	if count > maxRoll {
+		count = maxRoll
+	}
+
+	if count == 0 {
+		return
+	}
+
+	// delete old rows
+	start := 0 + fb.DS.GetScrollingRegionBottomRow() + 1 - count
+	fb.erase(start, count)
+
+	// insert new rows
+	start = 0 + beforeRow
+	fb.insert(start, count)
+}
+
+func (fb *Framebuffer) DeleteLine(row, count int) {
+	// invalide row
+	if row < fb.DS.GetScrollingRegionTopRow() || row > fb.DS.GetScrollingRegionBottomRow() {
+		return
+	}
+
+	maxRoll := fb.DS.GetScrollingRegionBottomRow() + 1 - row
+	if count > maxRoll {
+		count = maxRoll
+	}
+
+	if count == 0 {
+		return
+	}
+
+	// delete old rows
+	start := 0 + row
+	fb.erase(start, count)
+
+	// insert new rows
+	start = 0 + fb.DS.GetScrollingRegionBottomRow() + 1 - count
+	fb.insert(start, count)
+}
+
+func (fb *Framebuffer) erase(start int, count int) {
+	// delete count rows
+	copy(fb.rows[start:], fb.rows[start+count:])
+	fb.rows = fb.rows[:len(fb.rows)-count]
+}
+
+func (fb *Framebuffer) insert(start int, count int) {
+	// insert count rows
+	fb.rows = append(fb.rows[:start+count], fb.rows[start:]...)
+
+	// fill the row : copy or pointer?
+	for i := start; i < start+count; i++ {
+		fb.rows[i] = fb.newRow()
+	}
+
+	// remove the extra one
+	fb.rows = fb.rows[:len(fb.rows)-count]
 }
