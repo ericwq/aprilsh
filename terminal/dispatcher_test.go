fc7545dd4d2d4b85737cb5a26dec9f7aad3d7fca
ericwq <ericwq057@qq.com>
2022-05-04 22:35:07 +0800

    add dispatcher test.

---
+++ terminal/dispatcher_test.go
@@ -0,0 +1,66 @@
+/*
+
+MIT License
+
+Copyright (c) 2022 wangqi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+package terminal
+
+import (
+	"testing"
+)
+
+func TestDispatcherGetParam(t *testing.T) {
+	tc := []struct {
+		name   string
+		params string
+		want   []int
+	}{
+		{"normal", "4;57;", []int{4, 57}},
+		{"normal", ";5;58", []int{5, 58}},
+		{"abnormal", ";12;45;", []int{12, 45}},
+		{"too large", "65536;4500;", []int{0, 4500}},
+	}
+
+	for _, v := range tc {
+		// prepare the test case
+		d := Dispatcher{}
+		d.params.WriteString(v.params)
+		// t.Logf("%v\n", d.params)
+
+		d.parseAll()
+
+		for i := range d.parsedParams {
+			got := d.getParam(i, 0)
+			if i < len(v.want) {
+				if v.want[i] == got {
+					continue
+				} else {
+					t.Errorf("%s:\t case:%s\t [%02d parameter]: expect %d, got %d\n", v.name, v.params, i, v.want[i], got)
+				}
+			} else {
+				t.Errorf("%s:\t case:%s\t got too much result as wanted.", v.name, v.params)
+			}
+		}
+	}
+}
