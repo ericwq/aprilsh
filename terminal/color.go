b8db18935f04b8856d88076143bf0828d4262389
Qi Wang <ericwq057@qq.com>
2022-06-01 14:31:25 +0800

    add color.go

---
+++ terminal/color.go
@@ -0,0 +1,7 @@
+package terminal
+
+// https://github.com/gdamore/tcell/blob/master/color.go
+// https://www.ditig.com/256-colors-cheat-sheet
+var xterm256color = [...]uint32{
+	0x00000, 0x800000, 0x008000, 0x808000,
+}
