ce2a8414a978efcf5089cd017e21f7530124cb2c
Qi Wang <ericwq057@qq.com>
2022-08-30 13:46:37 +0800

    add Base64Key and test cases. export prngUint8() to PrngUint8().

--- encrypt/base64.go
+++ encrypt/base64.go
@@ -28,6 +28,8 @@ package encrypt
 
 import (
 	"crypto/rand"
+	"encoding/base64"
+	"fmt"
 )
 
 // use sys call to generate random number
@@ -45,10 +47,55 @@ func prngFill(size int) (dst []byte) {
 	return
 }
 
-func prngUint8() uint8 {
+func PrngUint8() uint8 {
 	var u8 uint8
 
 	dst := prngFill(1)
 	u8 = dst[0]
 	return u8
+}
+
+type Base64Key struct {
+	key []uint8
+}
+
+// random key
+func NewBase64Key() *Base64Key {
+	b := &Base64Key{}
+	b.key = prngFill(16)
+	return b
+}
+
+func NewBase64Key2(printableKey string) *Base64Key {
+	defer func() {
+		if err := recover(); err != nil {
+			return
+		}
+	}()
+
+	key, err := base64.StdEncoding.DecodeString(printableKey)
+	if err != nil {
+		panic(fmt.Sprintf("Key must be well-formed base64. %s\n", err))
+	}
+
+	if len(key) != 16 {
+		panic("Key must represent 16 octets.")
+	}
+
+	b := &Base64Key{}
+	b.key = key
+	// // to catch changes after the first 128 bits
+	// if printableKey != b.printableKey() {
+	// 	panic("Base64 key was not encoded 128-bit key.")
+	// }
+
+	return b
+}
+
+func (b *Base64Key) printableKey() string {
+	return base64.StdEncoding.EncodeToString(b.key)
+}
+
+func (b *Base64Key) data() []uint8 {
+	return b.key
 }
--- encrypt/base64_test.go
+++ encrypt/base64_test.go
@@ -1,6 +1,7 @@
 package encrypt
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -15,9 +16,34 @@ func TestPrng(t *testing.T) {
 	}
 
 	for i := 0; i < 8; i++ {
-		got := prngUint8()
+		got := PrngUint8()
 		if got == 0 {
 			t.Errorf("prngUint8 got %#v\n", got)
 		}
+	}
+}
+
+func TestBase64Key(t *testing.T) {
+	// normal key
+	normalKey := NewBase64Key()
+	printKey := normalKey.printableKey()
+	gotNormal := NewBase64Key2(printKey)
+	if !reflect.DeepEqual(normalKey.data(), gotNormal.data()) {
+		t.Errorf("two keys should be the same. got key1=\n%v, key2=\n%v\n", normalKey, gotNormal)
+	}
+
+	// malform key
+	malformBase64 := "/msvMB1KwXL+ygJHdJwwQ=="
+	malformKey := NewBase64Key2(malformBase64)
+	if malformKey != nil {
+		t.Error("malform key should be nil.")
+	}
+
+	// key length is short
+	shortLengthKey := &Base64Key{key: prngFill(8)}
+	key4 := NewBase64Key2(shortLengthKey.printableKey())
+	if key4 != nil {
+		t.Error("key length is short.")
+		t.Errorf("key length is short. %q\n", shortLengthKey.printableKey())
 	}
 }
