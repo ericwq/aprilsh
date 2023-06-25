d59dc541fba989678f637862eb6784d52b180c54
ericwq <ericwq057@qq.com>
2023-06-25 15:49:34 +0800

    - add aprilsh-client directory.

---
+++ cmd/aprilsh-client/client.go
@@ -0,0 +1,11 @@
+// Copyright 2022~2023 wangqi. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("aprilsh client start")
+}
