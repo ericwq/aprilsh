ad2294e09b8a44e0f737ccfbdc86770ea84a5618
Qi Wang <ericwq057@qq.com>
2023-07-18 14:02:36 +0800

    - split signal_test.go to two files. one for darwin, one for linux.

--- frontend/signal_darwin_test.go
+++ frontend/signal_darwin_test.go
@@ -11,43 +11,50 @@ import (
 )
 
 /*
-./signal_test.go:15:78: undefined: syscall.SIGCLD
-./signal_test.go:17:76: undefined: syscall.SIGPOLL
-./signal_test.go:18:28: undefined: syscall.SIGPWR
-./signal_test.go:18:78: undefined: syscall.SIGSTKFLT
-./signal_test.go:20:45: undefined: syscall.SIGUNUSED
+darwin does not has the following const
 
+	syscall.SIGCLD
+	syscall.SIGPOLL
+	syscall.SIGPWR
+	syscall.SIGSTKFLT
+	syscall.SIGUNUSED
 */
 func TestGotSignal(t *testing.T) {
 	tc := []os.Signal{
-		syscall.SIGABRT, syscall.SIGALRM, syscall.SIGBUS, syscall.SIGCHLD, syscall.SIGCLD,
+		syscall.SIGABRT, syscall.SIGALRM, syscall.SIGBUS, syscall.SIGCHLD,
 		syscall.SIGCONT, syscall.SIGFPE, syscall.SIGHUP, syscall.SIGILL, syscall.SIGINT,
-		syscall.SIGIO, syscall.SIGIOT, syscall.SIGKILL, syscall.SIGPIPE, syscall.SIGPOLL,
-		syscall.SIGPROF, syscall.SIGPWR, syscall.SIGQUIT, syscall.SIGSEGV, syscall.SIGSTKFLT,
+		syscall.SIGIO, syscall.SIGIOT, syscall.SIGKILL, syscall.SIGPIPE,
+		syscall.SIGPROF, syscall.SIGQUIT, syscall.SIGSEGV,
 		syscall.SIGSTOP, syscall.SIGSYS, syscall.SIGTERM, syscall.SIGTRAP, syscall.SIGTSTP,
-		syscall.SIGTTIN, syscall.SIGTTOU, syscall.SIGUNUSED, syscall.SIGURG, syscall.SIGUSR1,
+		syscall.SIGTTIN, syscall.SIGTTOU, syscall.SIGURG, syscall.SIGUSR1,
 		syscall.SIGUSR2, syscall.SIGVTALRM, syscall.SIGWINCH, syscall.SIGXCPU, syscall.SIGXFSZ,
 	}
 
-	repeat := []bool{
-		true, true, true, true, false,
+	/*
+	   syscall.SIGIOT and syscall.SIGABRT has conflict value
+	*/
+	result := []bool{
+		true, true, true, true,
 		true, true, true, true, true,
-		true, false, true, true, false,
+		true, false, true, true,
+		true, true, true,
 		true, true, true, true, true,
-		true, true, true, true, true,
-		true, true, false, true, true,
+		true, true, true, true,
 		true, true, true, true, true,
 	}
+
+	// initialize Signals array
 	var s Signals
 	for i := range tc {
 		s.Handler(tc[i])
 	}
 
+	// validate GotSignal()
 	for i := range tc {
 		ss := tc[i].(syscall.Signal)
 		got := s.GotSignal(ss)
-		if got != repeat[i] {
-			t.Errorf("#test GotSignal() %q %x expect %t, got %t\n", tc[i], int(ss), repeat[i], got)
+		if got != result[i] {
+			t.Errorf("#test GotSignal() %q %x expect %t, got %t\n", tc[i], int(ss), result[i], got)
 		}
 	}
 }
