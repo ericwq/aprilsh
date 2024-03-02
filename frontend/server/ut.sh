1331eb58bbff74c3d8c05cae863d108b6336bbce
Qi Wang <ericwq057@qq.com>
2024-03-02 08:51:51 +0800

    add ut.sh for server.

---
+++ frontend/server/ut.sh
@@ -0,0 +1,6 @@
+go test -cover . -args -test.gocoverdir=./coverage/unit 
+go tool covdata percent -i=./coverage/unit,./coverage/int 
+go tool covdata textfmt -i=./coverage/unit,./coverage/int -o coverage/profile
+go tool cover -func coverage/profile
+
+echo "go tool cover -html coverage/profile"
