d0e9c6f440b05e1ba4456995f5d4f2ec3515dbe6
ericwq <ericwq057@qq.com>
2024-03-30 14:49:45 +0800

    refine ldflags with clean code. #31. change _git_commit to short version. refine build.sh (both server and client) for clean ldflags.

--- frontend/client/build.sh
+++ frontend/client/build.sh
@@ -10,31 +10,35 @@ rm -rf coverage
 mkdir -p coverage/unit 
 mkdir -p coverage/int
 
+# selecting package for integration coverage test
+PKGS="github.com/ericwq/aprilsh/frontend/client"
+
+# prepare for ldflags
+_module_name=$(head ../../go.mod | grep '^module' | awk '{print $2}')
+_go_version=$(go version | grep 'version' | awk '{print $3}')
+_git_tag=$(git describe --tags)
+_git_commit=$(git rev-parse --short HEAD)
+_git_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# set ldflags
+ldflags="-s -w \
+	-X $_module_name/frontend.BuildTime=$(date -u +%Y-%m-%dT%H:%M:%S) \
+	-X $_module_name/frontend.GitBranch=$_git_branch \
+	-X $_module_name/frontend.GitCommit=$_git_commit \
+	-X $_module_name/frontend.GitTag=$_git_tag \
+	-X $_module_name/frontend.GoVersion=$_go_version \
+	"
+# required for github.com/docker/docker
+export GO111MODULE=auto
+
+# build client for test
+echo "build client start: $(date '+%F %T')"
+go build -cover -coverpkg=$PKGS -ldflags="$ldflags" -o ~/.local/bin/apsh
+echo "build client end  : $(date '+%F %T')"
+echo "output client to  : ~/.local/bin/apsh"
+echo "run with          : GOCOVERDIR=./coverage/int  ~/.local/bin/apsh -verbose ide@localhost"
+
 # chech version number
 # if [ "$1" != "" ]; then Version=$1;else
 #     read -p "Input Build Version: " Version; if [ "$Version" = "" ]; then echo "The input cannot be empty";exit;fi
 # fi
-echo "build client start: $(date '+%F %T')"
-#
-# selecting package to cover
-PKGS="github.com/ericwq/aprilsh/frontend/client"
-
-# get go module name
-_module_name=$(head ../../go.mod | grep "^module" | awk '{print $2}')
-_build_time=$(date '+%F %T')
-_go_version=$(go version | grep "version" | awk '{print $3,$4}')
-_git_tag=$(git describe --tags)
-_git_commit=$(git rev-parse HEAD)
-_git_branch=$(git rev-parse --abbrev-ref HEAD)
-#
-# build server for test
-go build -cover -coverpkg=$PKGS -ldflags="-s -w
-      -X '${_module_name}/frontend.GitTag=${_git_tag}'
-      -X '${_module_name}/frontend.GoVersion=${_go_version}'
-      -X '${_module_name}/frontend.GitCommit=${_git_commit}'
-      -X '${_module_name}/frontend.GitBranch=${_git_branch}'
-      -X '${_module_name}/frontend.BuildTime=${_build_time}'" -o ~/.local/bin/apsh .
-# go build -race -cover -coverpkg=$PKGS -o ~/.local/bin/apshd .
-echo "build client end  : $(date '+%F %T')"
-echo "output client to  : ~/.local/bin/apsh"
-echo "run with          : GOCOVERDIR=./coverage/int  ~/.local/bin/apsh -verbose ide@localhost"
--- frontend/server/build.sh
+++ frontend/server/build.sh
@@ -10,30 +10,35 @@ rm -rf coverage
 mkdir -p coverage/unit 
 mkdir -p coverage/int
 
-echo "build server start: $(date '+%F %T')"
-#
-# selecting package to cover
+# selecting package for integration coverage test
 PKGS="github.com/ericwq/aprilsh/frontend/server"
 
 # prepare for ldflags
-_module_name=$(head ../../go.mod | grep "^module" | awk '{print $2}')
-_build_time=$(date '+%F %T')
-_go_version=$(go version | grep "version" | awk '{print $3,$4}')
+_module_name=$(head ../../go.mod | grep '^module' | awk '{print $2}')
+_go_version=$(go version | grep 'version' | awk '{print $3}')
 _git_tag=$(git describe --tags)
-_git_commit=$(git rev-parse HEAD)
+_git_commit=$(git rev-parse --short HEAD)
 _git_branch=$(git rev-parse --abbrev-ref HEAD)
-#
+
+# set ldflags
+ldflags="-s -w \
+	-X $_module_name/frontend.BuildTime=$(date -u +%Y-%m-%dT%H:%M:%S) \
+	-X $_module_name/frontend.GitBranch=$_git_branch \
+	-X $_module_name/frontend.GitCommit=$_git_commit \
+	-X $_module_name/frontend.GitTag=$_git_tag \
+	-X $_module_name/frontend.GoVersion=$_go_version \
+	"
+# required for github.com/docker/docker
+export GO111MODULE=auto
+
 # build server for test
-go build -cover -coverpkg=$PKGS -ldflags="-s -w
-      -X '${_module_name}/frontend.GitTag=${_git_tag}'
-      -X '${_module_name}/frontend.GoVersion=${_go_version}'
-      -X '${_module_name}/frontend.GitCommit=${_git_commit}'
-      -X '${_module_name}/frontend.GitBranch=${_git_branch}'
-      -X '${_module_name}/frontend.BuildTime=${_build_time}'" -o ~/.local/bin/apshd .
-# go build -race -cover -coverpkg=$PKGS -o ~/.local/bin/apshd .
+echo "build server start: $(date '+%F %T')"
+go build -cover -coverpkg=$PKGS -ldflags="$ldflags" -o ~/.local/bin/apshd
 echo "build server end  : $(date '+%F %T')"
 echo "output server to  : ~/.local/bin/apshd"
 
+# for linux sudo copy apshed to /usr/bin
+# for darwin just run it.
 _osType=$(uname)
 if [ "${_osType}" == 'Linux' ]
 then
@@ -47,4 +52,3 @@ then
 fi
 
 echo "run with          : GOCOVERDIR=./coverage/int apshd -verbose 2> /tmp/apshd.log"
-export APRILSH_APSHD_PATH=~/.local/bin/apshd
