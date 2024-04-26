36e718780e2d231e5cf9736faa016113b1c7066b
Qi Wang <ericwq057@qq.com>
2024-04-26 18:07:41 +0800

    add build tags for server.

--- frontend/server/build.sh
+++ frontend/server/build.sh
@@ -20,6 +20,22 @@ _git_tag=$(git describe --tags)
 _git_commit=$(git rev-parse --short HEAD)
 _git_branch=$(git rev-parse --abbrev-ref HEAD)
 
+_osType=$(uname)
+if [ "${_osType}" == 'Darwin' ]
+then
+_musl=$(otool -L /bin/ls |  grep 'musl'|head -n 1| awk '{print $1}')
+else
+_musl=$(ldd /bin/ls | grep 'musl'|head -n 1| awk '{print $1}')
+fi
+
+if [ "$_musl" == "" ]
+then
+	_build_tag="-tags utmp"
+else
+	_build_tag="-tags utmps"
+fi
+echo "use tags: $_build_tag, musl: $_musl"
+
 # set ldflags
 ldflags="-s -w \
 	-X $_module_name/frontend.BuildTime=$(date -u +%Y-%m-%dT%H:%M:%S) \
@@ -33,22 +49,15 @@ export GO111MODULE=auto
 
 # build server for test
 echo "build server start: $(date '+%F %T')"
-go build -cover -coverpkg=$PKGS -ldflags="$ldflags" -o ~/.local/bin/apshd
+go build $_build_tag -cover -coverpkg=$PKGS -ldflags="$ldflags" -o ~/.local/bin/apshd
 echo "build server end  : $(date '+%F %T')"
 echo "output server to  : ~/.local/bin/apshd"
 
 # for linux sudo copy apshed to /usr/bin
-# for darwin just run it.
-_osType=$(uname)
 if [ "${_osType}" == 'Linux' ]
 then
   echo "copy server to    : /usr/bin/apshd"
   sudo cp ~/.local/bin/apshd /usr/bin/apshd
-fi
-if [ "${_osType}" == 'Darwin' ]
-then
-	# run it for the first time for macOS
-  ~/.local/bin/apshd --version
 fi
 
 echo "run with          : GOCOVERDIR=./coverage/int apshd -verbose 2> /tmp/apshd.log"
